937aada21403a1c88a1b304798bcc9ac0c140bdc
adrianobrito <adriano.brito@ifactory.com>
2017-08-29 15:28:59 -0300

    Create spec for memory database

---
+++ test/database/memory-database-spec.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+describe('MemoryDatabase', () => {
+
+  describe('#get()', () => {
+    it('should return all values from a specific endpoint', () => {});
+    it('should return a specific object', () => {});
+  });
+
+  describe('#post()', () => {
+    it('should return an object with a non-null id property', () => {});
+  });
+
+  describe('#put()', () => {
+    it('should return an updated version of an specific object from an endpoint', () => {});
+  });
+
+  describe('#delete()', () => {
+    it('should return an the removed object', () => {});
+  });
+
+});
