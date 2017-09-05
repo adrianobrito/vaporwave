c1746c2bf7e3cb35e4b30bd14b0547dd96bb8705
adrianobrito <adriano.brito@ifactory.com>
2017-09-05 13:26:30 -0300

    Refactor old components with ES6 syntax

---
+++ src/request/request-parser.js
@@ -0,0 +1,38 @@
+const RequestParser = (() => {
+    return {
+        extractRequestData(request) {
+            return {
+                endpoint : extractEndpoint(request),
+                method   : extractMethod(request),
+                body     : extractBody(request)
+            };
+        }
+    };
+
+    function extractEndpoint(request) {
+        console.log("[INFO] Parsing incoming request");
+        var url         = request.url;
+        var urlRegex    = /([\/A-Za-z+]+)(\/[\d+]*)/;
+        var groups      = urlRegex.exec(url);
+        var lastUrlPath = url.replace(urlRegex, '$2').replace('/','');
+
+        return isDigit(lastUrlPath) ?
+            {entity: url.replace(urlRegex, '$1'), id: parseInt(lastUrlPath)} :
+            {entity: url};
+    }
+
+    function extractBody(request) {
+        var body = request.body;
+        return body && JSON.parse(body) || undefined;
+    }
+
+    function extractMethod(request) {
+        return request.method;
+    }
+
+    function isDigit(str) {
+        return /^\d+$/.test(str);s
+    }
+})();
+
+export default RequestParser;
