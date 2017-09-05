786fdfaa60f952e3821b8626360ca343905f0ab6
adrianobrito <adriano.brito@ifactory.com>
2017-09-05 13:37:49 -0300

    Add help comments in new memory-database

--- src/database/memory-database.js
+++ src/database/memory-database.js
@@ -48,3 +48,72 @@ export default class MemoryDatabase extends Database {
 	}
 
 }
+/****
+OLD VERSION
+var dynamicIdGenerator = require('./dynamic-id-generator.js');
+
+(function (memoryDatabase) {
+
+	var memoryDatabaseObject = {};
+
+	memoryDatabase.get    = _get;
+	memoryDatabase.post   = _post;
+	memoryDatabase.put    = _put;
+	memoryDatabase.delete = _delete;
+	
+	function _get(requestObject){
+		console.log("[GET] Getting a resource in memory database");
+		var endpoint = requestObject.endpoint;
+		var targetCollection = memoryDatabaseObject[endpoint.entity] || [];
+		if(!endpoint.id) {
+			return targetCollection;
+		}
+
+		var targetObject = targetCollection.filter(byId(endpoint.id));
+		return targetObject.length && targetObject[0] || undefined;
+	}
+
+	function _post(requestObject){
+		console.log("[POST] Inserting a resource in memory database");
+		var endpoint = requestObject.endpoint;
+		var body     = requestObject.body;
+		if(!memoryDatabaseObject[endpoint.entity]) {
+			memoryDatabaseObject[endpoint.entity] = [];
+		}
+
+		body.id = dynamicIdGenerator.generateId();
+		memoryDatabaseObject[endpoint.entity].push(body);
+		return body;
+	}
+
+	function _put(requestObject){
+		console.log("[PUT] Updating a resource in memory database");
+		var body             = requestObject.body;
+		var endpoint         = requestObject.endpoint;
+		var targetCollection = memoryDatabaseObject[endpoint.entity] || [];
+		var targetIndex      = targetCollection.findIndex(byId(endpoint.id));
+
+		body.id 					  = endpoint.id;
+		targetCollection[targetIndex] = body;
+		return body;
+	}
+
+	function _delete(requestObject){
+		console.log("[DELETE] Removing a resource in memory database");
+		var endpoint         = requestObject.endpoint;
+		var targetCollection = memoryDatabaseObject[endpoint.entity] || [];
+		var targetIndex      = targetCollection.findIndex(byId(endpoint.id));
+		var deletedObject    = targetCollection[targetIndex];
+
+		targetCollection.splice(targetIndex, 1);
+		return deletedObject;
+	}
+
+	function byId(id) {
+		return function(object) {
+			return object.id === id;
+		}
+	}
+
+})(module.exports);
+**/
