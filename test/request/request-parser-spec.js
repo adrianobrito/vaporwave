c8f79f5e941f759d260fd258abb65619cf0bfa09
adrianobrito <adriano.brito@ifactory.com>
2017-09-06 12:41:20 -0300

    Add Request Parser Specs

--- test/request/request-parser-spec.js
+++ test/request/request-parser-spec.js
@@ -1,26 +1,55 @@
 import {expect} from 'chai';
+import RequestParser from '../../src/request/request-parser';
 
 describe('RequestParser', () => {
 
 	describe("#extractRequestData", () => {
+		const request = {
+			url: '/collection',
+			method: 'GET'
+		};
 
 		describe("when a request don't has id in path", () => {
-			it("parses to an object with an endpoint without id property");
+			const requestData = RequestParser.extractRequestData(request);
+
+			it("parses to an object with an endpoint without id property", () => {
+				expect(requestData).to.exist;
+				expect(requestData.endpoint).to.exist;
+				expect(requestData.endpoint.id).to.be.undefined;
+			});
 		});
 
 		describe("when a request has id in path", () => {
-			it("parses to an object with an endpoint cointaining id property");
+			request.url = "/collection/15";
+			const requestData = RequestParser.extractRequestData(request);
+
+			it("parses to an object with an endpoint cointaining id property", () => {
+				expect(requestData.endpoint.id).to.exist;
+			});
 		});
 
 		describe("when a request has body", () => {
-			it("parses to an object with non-null body property");
-		}
+			request.body = '{ "name" : "object" }';
+			const requestData = RequestParser.extractRequestData(request);
+
+			it("parses to an object with non-null body property", () => {
+				expect(requestData.body).to.exist;
+			});
+		});
 
 		describe("when a request don't has body", () => {
-			it("parses to an object without body property");
-		}
+			request.body = undefined;
+			const requestData = RequestParser.extractRequestData(request);
 
-		it("parses to an object with non-null method property");
+			it("parses to an object without body property", () => {
+				expect(requestData.body).to.be.undefined;
+			});
+		});
+
+		it("parses to an object with non-null method property", () => {
+			const requestData = RequestParser.extractRequestData(request);
+			expect(requestData.method).to.exist;
+		});
 
 	});
 
