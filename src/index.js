bca861e02e4fd68e6b67082365fd28021da6992b
Adriano Brito <adrianosdebrito@gmail.com>
2017-09-21 12:50:30 -0300

    Cleanup gh-pages branch

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Server from './server.js';
-import minimist from 'minimist';
-
-(() => {
-	const args = minimist(process.argv.slice(2));
-	Server.start(args.port && parseInt(args.port) || undefined);
-})();
