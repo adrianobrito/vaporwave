e6f429dcde63d2bb8fce8bd06e4ecfbe93dbb8b2
adrianobrito <adrianosdebrito@gmail.com>
2017-09-28 13:24:40 -0300

    Add some changes in initialization file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import Server from './server';
 import minimist from 'minimist';
+import JsonFileManager from './database/json-file-manager';
 
 (() => {
-	const args         = minimist(process.argv.slice(2));
-	const port         = args.port && parseInt(args.port) || undefined;
-	const isPersistent = args.hasOwnProperty('persistent');
-	const clearCache   = args.hasOwnProperty('clear-cache');
+	const args       = minimist(process.argv.slice(2));
+	const clearCache = args.hasOwnProperty('clear-cache');
+	const config     = {
+		port    : args.port && parseInt(args.port) || undefined,
+		mode    : args.hasOwnProperty('persistent') ? 'persistent' : 'default',
+		fixture : args.fixture && loadFixture(args.fixture) || undefined
+	};
 
 	if(clearCache) {
 		Server.clearCache();
 	} else {
-		Server.start(port, isPersistent);
+		Server.start(config);
+	}
+
+	function loadFixture(path) {
+		if(!path) {
+			throw "You must specify path location when you use --fixture";
+		}
+
+		if(path && (path[0] === '/' || path[1] === ':')) {
+			return JsonFileManager.load(path);
+		} else {
+			const relativePath = process.cwd();
+			return JsonFileManager.load(`${relativePath}/${path}`);
+		}
 	}
 })();
